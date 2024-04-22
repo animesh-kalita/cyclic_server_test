03bd1f8df2ee586a473b4506ce637e71fd3fcd65
Animesh Kalita <animesh_k@lattech.in>
2024-04-22 18:22:45 +0530

    Get Route Added For Home Route

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 
 // Routes
 app.use('/tours', tourRoutes);
-app.get("/",app.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Welcome to Adventure Tours API");
-}))
+});
 // Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
